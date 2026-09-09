99481b89bb66bdf34bcd914cc008950d3b3c7069
FrancoBanis <franco.banis0@gmail.com>
2026-09-09 00:54:54 -0300

    refactor: cambie el config.ts , ahora lo que se almacena en el .env  esta en el config.

--- frontend/src/constants/config.ts
+++ frontend/src/constants/config.ts
@@ -1,6 +1,6 @@
 import type { UsuarioData } from "./usuarioData"
 
-export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+export const BACKEND_URL = "http://localhost:8080"
 
 export const ROUTES = {
     HOME: '/',
